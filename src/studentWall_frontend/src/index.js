f6fb8bf398c9967e6a2d47c289c4101d7d8a5985
vramva <vavram@pm.me>
2023-05-12 19:56:45 +0300

    Initial commit.

---
+++ src/studentWall_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { studentWall_backend } from "../../declarations/studentWall_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await studentWall_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
