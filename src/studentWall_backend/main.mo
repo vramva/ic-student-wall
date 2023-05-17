b8536e5d3c9cc3d96331a67ed4dd45b2a250f374
vramva <vavram@pm.me>
2023-05-17 21:42:25 +0300

    Exclude regeneratable code

--- src/studentWall_backend/main.mo
+++ src/studentWall_backend/main.mo
@@ -1,5 +1,146 @@
+import HashMap "mo:base/HashMap";
+import Hash "mo:base/Hash";
+import Nat "mo:base/Nat";
+import Principal "mo:base/Principal";
+import Result "mo:base/Result";
+import Order "mo:base/Order";
+import Int "mo:base/Int";
+import Buffer "mo:base/Buffer";
+import Text "mo:base/Text";
+import Iter "mo:base/Iter";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
-};
+    type Content = {
+        #Text : Text;
+        #Image: Blob;
+        #Video : Blob;
+    };
+    type Message = {
+        vote : Int;
+        content : Content;
+        creator : Principal;
+        username : Text;
+    };
+    var messageId = 0;  
+    var wall = HashMap.HashMap<Nat, Message>(0, Nat.equal, Hash.hash);
+    private stable var wallEntries : [(Nat, Message)] = [];
+    private stable var wallSize = 0;
+    // Add a new message to the wall
+    public shared (message) func writeMessage(c : Content, user : Text) : async Nat {
+        let pid = message.caller;
+        let msg : Message = {vote = 0; content = c; creator = pid; username = user};
+        messageId += 1;
+        wall.put(messageId, msg);
+        return messageId;
+    };
+
+    public shared query(message) func greet(name : Text) : async Text {
+      let caller = message.caller;
+      return "Hello, " # name # "! " # "Your Principal Id is: " # Principal.toText(caller);
+   };
+
+    //Get a specific message by ID
+    public shared query func getMessage(messageId : Nat) : async Result.Result<Message, Text> {
+        let msg = wall.get(messageId);
+            switch (msg) {
+                case null   return #err("Message not found");
+                case (?msg) return #ok(msg);
+                };
+
+    };
+
+    // Update the content for a specific message by ID
+    public shared (message) func updateMessage(messageId: Nat, c: Content) : async Result.Result<(), Text> {
+        let pid = message.caller;
+        var msg = wall.get(messageId);
+            switch (msg) {
+                case null   return #err("Message not found");
+                case (?msg) {
+                    if (pid == msg.creator) {
+                        let m = {vote = msg.vote; content = c; creator = msg.creator; username = msg.username};
+                        ignore wall.replace(messageId, m);
+                        return #ok();
+                    };
+                    return #err("Principal ID doesn't match");
+            };
+        };
+    };
+    
+    
+
+ 
+    //Delete a specific message by ID
+    public shared func deleteMessage(messageId: Nat) : async Result.Result<(), Text> {
+        let msg = wall.get(messageId);
+        switch (msg) {
+                case null   return #err("Message not found");
+                case (?msg) return #ok(wall.delete(messageId));
+        }
+    };
+ 
+    // Voting
+    public shared func upVote(messageId: Nat) : async Result.Result<(), Text> {
+        let msg = wall.get(messageId);
+        switch (msg) {
+            case null return #err("Message not found");
+            case (?msg) {
+                let m = {vote = msg.vote + 1; content = msg.content; creator = msg.creator; username = msg.username};
+                ignore wall.replace(messageId, m);
+                return #ok();
+            };
+        }
+    };
+
+    public shared func downVote(messageId: Nat) : async Result.Result<(), Text> {
+        let msg = wall.get(messageId);
+        switch (msg) {
+            case null return #err("Message not found");
+            case (?msg) {
+                let m = {vote = msg.vote - 1; content = msg.content; creator = msg.creator; username = msg.username};
+                ignore wall.replace(messageId, m);
+                return #ok();
+            };
+        }
+    }; 
+
+    //Get all messages
+    public shared query func getAllMessages() : async [Message] {
+        var messages = Buffer.Buffer<Message>(0);
+        for (key in wall.keys()) {
+            switch (wall.get(key)){
+                case(?msg) messages.add(msg);
+                case(_) return [];
+            }
+        };
+        return Buffer.toArray(messages);
+    };
+
+    //Messages compare
+    private func compareVotes(m1 : Message, m2 :Message) : Order.Order {
+        switch(Int.compare(m1.vote, m2.vote)) {
+            case(#greater) return #less;
+            case(#less) return #greater;
+            case(_) return #equal;
+        }
+    };
+
+    //Get all messages ordered by votes
+    public shared query func getAllMessagesRanked() : async [Message] {
+        var messagesRanked = Buffer.Buffer<Message>(0);
+        for (msg in wall.vals()) {
+            messagesRanked.add(msg);
+        };
+        messagesRanked.sort(compareVotes);
+        return Buffer.toArray(messagesRanked);
+    };
+
+    system func preupgrade() {
+        wallEntries := Iter.toArray(wall.entries());
+        wallSize := wall.size(); 
+    };
+
+    system func postupgrade() {
+        wall := HashMap.fromIter<Nat, Message>(wallEntries.vals(), wallSize, Nat.equal, Hash.hash);
+    }
+}
+
--- src/studentWall_frontend/src/index.js
+++ src/studentWall_frontend/src/index.js
@@ -1,19 +1,217 @@
 import { studentWall_backend } from "../../declarations/studentWall_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { Actor } from "@dfinity/agent";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+let authClient = null;
+let principalId = localStorage.getItem('principalId') || "";
+let username = localStorage.getItem('username') || "anon";
 
-  const name = document.getElementById("name").value.toString();
+async function init() {
+    authClient = await AuthClient.create();
+    if (principalId != "") {
+      Actor.agentOf(studentWall_backend).replaceIdentity(authClient.getIdentity());
+    }
+    getMessages();
+}
 
+//Event Handlers
+
+document.addEventListener("DOMContentLoaded", ready);
+
+document.getElementById("logout").addEventListener("click", async (e) => {
+  principalId = "";
+  localStorage.setItem('principalId', principalId);
+  document.getElementById("writeMsg").style.display = "none";
+  document.getElementById("post").style.display = "none";
+  document.getElementById("textarea").style.display = "none";
+  showDashboard();
+  showLogin(true);
+});
+
+document.getElementById("writeMsg").addEventListener("click", async (e) => {
+  if (  document.getElementById("post").style.display != "none" && document.getElementById("textarea").style.display != "none") {
+    document.getElementById("textarea").style.display = "none";
+    document.getElementById("textarea").style.transition = "width 2s, height 2s, background-color 2s";
+    document.getElementById("post").style.display = "none";
+    document.getElementById("post").style.transition = "width 2s, height 2s, background-color 2s";
+  } else {
+    document.getElementById("textarea").style.display = "flex";
+    document.getElementById("textarea").style.transition = "width 2s, height 2s, background-color 2s";
+    document.getElementById("post").style.display = "flex";
+    document.getElementById("post").style.transition = "width 2s, height 2s, background-color 2s";
+  }
+});
+
+document.getElementById("textarea").addEventListener("focus", async (e) => {
+  if (document.getElementById("textarea").value == "...") {
+  document.getElementById("textarea").value = "";
+  }
+});
+
+document.getElementById("post").addEventListener("click", async (e) => {
+
+  const button = document.getElementById("post");
   button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await studentWall_backend.greet(name);
-
+  
+  // send message to backend
+  const message = document.getElementById("textarea").value;
+  let content = {Text : message};
+  let msgId = await studentWall_backend.writeMessage(content, username);
+  
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+  document.getElementById("textarea").value = "";
+  document.getElementById("post").style.display = "none";
+  document.getElementById("textarea").style.display = "none";
+});
+
+
+
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+  //const button = e.target.querySelector("button");
+
+  
+
+  //button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+
+  if (!authClient) throw new Error("AuthClient not initialized");
+
+  authClient.login({
+    onSuccess: handleSuccess,
+  });
+
+  const greeting = await studentWall_backend.greet(username);
+
+  //button.removeAttribute("disabled");
 
   return false;
 });
+
+function handleSuccess() {
+  principalId = authClient.getIdentity().getPrincipal().toText();
+  if (document.getElementById("name").value != "") {  
+    username = document.getElementById("name").value;
+  } 
+  localStorage.setItem('principalId', principalId);
+  localStorage.setItem('username', username);
+  showLogin(false);
+  showDashboard();
+
+  document.getElementById("principalId").innerText = `Principal Id: ${principalId}`;
+  document.getElementById("username").innerText = `${username}`;
+  document.getElementById("writeMsg").style.display = "block";
+
+  Actor.agentOf(studentWall_backend).replaceIdentity(authClient.getIdentity());
+}
+
+
+// UI functions
+function ready() {
+  showDashboard();
+  if (principalId != "") {
+    showLogin(false);
+    document.getElementById("principalId").innerText = `Principal Id: ${principalId}`;
+    document.getElementById("username").innerText = `${username}`;
+    document.getElementById("writeMsg").style.display = "block";
+  } else {
+    showLogin(true);
+  }
+};
+
+async function getMessages() {
+  var messagesBuffer = await studentWall_backend.getAllMessages();
+  loadMessage(messagesBuffer);
+}
+
+function loadMessage(messages) {
+  for (let msg of messages) {
+    let user = msg.username;
+    let pid = msg.creator;
+    let m = parseContent(msg.content);
+    //var html = `<div>User: ${user}  Principal Id: ${pid}  Message: ${m}</div>`;
+    var div = document.createElement('div');
+    div.className = `postContainer`;
+    var html = '<div class="card">';
+    html += `  <img id="avatar" src="motoko.gif" alt="Avatar" style="width:100%">`;
+    html += '  <div class="container">';
+    html += `      <h4 style="color: coral"><b>${user}</b></h4> `;
+    html += `      <p>Principal Id: ${pid}</p>`;
+    html += `  </div>`;
+    html += `</div>`;
+    html += `<div class="postarea">${m}</div>`;
+    div.innerHTML = html;
+    document.getElementById("wall").appendChild(div);
+  }
+}
+
+function parseContent(content) {
+  let str = JSON.stringify(content);
+  return str.split("\"")[3];
+}
+
+function showLogin(val) {
+  if (val === true) {
+    const loginElems = document.getElementsByClassName("loginContainer");
+    for (var i = 0; i < loginElems.length; i++) {
+      loginElems[i].style.display = "block";
+    }
+  } else {
+    const loginElems = document.getElementsByClassName("loginContainer");
+    for (var i = 0; i < loginElems.length; i++) {
+      loginElems[i].style.display = "none";
+    }
+  }
+}
+
+function showDashboard() {
+  if (principalId != "") {
+    const Elems = document.getElementsByClassName("dashboardContainer");
+    for (var i = 0; i < Elems.length; i++) {
+      Elems[i].style.display = "flex";
+    }
+  } else {
+    const Elems = document.getElementsByClassName("dashboardContainer");
+    for (var i = 0; i < Elems.length; i++) {
+      Elems[i].style.display = "none";
+    }
+  }
+}
+
+
+
+
+/* document.getElementById("msgBtn").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const button = document.getElementById("msgBtn");
+
+  const myMessage = document.getElementById("msg").value.toString();
+
+  button.setAttribute("disabled", true);
+  // Write message to the wall and display it in the textbox
+  const msgId = await studentWall_backend.writeMessage({Text : myMessage});
+  const returnVal = await studentWall_backend.getMessage(msgId);
+  const msg = JSON.stringify(returnVal['ok'].content);
+  document.getElementById("msg").value = "Message: " + msg.split("\"")[3];
+
+  button.removeAttribute("disabled");
+
+  
+
+  return false;
+});
+ */
+
+/* document.getElementById("login").addEventListener("click", async (e) => {
+  if (!authClient) throw new Error("AuthClient not initialized");
+
+  authClient.login({
+    onSuccess: handleSuccess,
+  });
+}); */
+
+init();
